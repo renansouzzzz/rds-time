c7ea2683440e10b2c93d35839beae75854ae1a28
renansouzzzz <renansouza.oliv@outlook.com>
2024-01-25 10:48:15 -0300

    script rds

--- rds-instance.sh
+++ rds-instance.sh
@@ -1,18 +1,19 @@
 instance_rds = ""
+instance_ec2 = ""
 profile = ""
 region = ""
 
 
 start_ec2() {
-    echo "Startando instância $instance no exato momento!"
-    aws ec2 start-instances --instance-ids --region $region --profile $profile
-    echo "Instância $instance startada com sucesso!"
+    echo "Startando instância $instance_ec2 no exato momento!"
+    aws ec2 start-instances --instance-ids $instance_ec2 --region $region --profile $profile
+    echo "Instância $instance_ec2 startada com sucesso!"
 }
 
 stop_ec2() {
-    echo "Finalizando instância $instance!"
-    aws ec2 stop-instances --instance-ids --region $region --profile $profile
-    echo "Instância $instance startada com sucesso!"
+    echo "Finalizando instância $instance_ec2!"
+    aws ec2 stop-instances --instance-ids $instance_ec2 --region $region --profile $profile
+    echo "Instância $instance_ec2 startada com sucesso!"
 }
 
 start_rds() {
