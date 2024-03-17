1417230fd4ea084ec7c3ef5434e880aa0775659b
Renan de Oliveira <85078612+renansouzzzz@users.noreply.github.com>
2024-03-17 11:37:31 -0300

    rds-instance.sh

--- rds-instance.sh
+++ rds-instance.sh
@@ -1,8 +1,10 @@
-instance_rds = ""
-instance_ec2 = ""
-profile = ""
-region = ""
+#!/bin/bash
 
+instance_ec2=""
+region=""
+profile=""
+
+current_hour=$(date +%H)
 
 start_ec2() {
     echo "Startando instância $instance_ec2 no exato momento!"
@@ -13,15 +15,11 @@ start_ec2() {
 stop_ec2() {
     echo "Finalizando instância $instance_ec2!"
     aws ec2 stop-instances --instance-ids $instance_ec2 --region $region --profile $profile
-    echo "Instância $instance_ec2 startada com sucesso!"
+    echo "Instância $instance_ec2 finalizada com sucesso!"
 }
 
-start_rds() {
-    aws rds start-db-instance --db-instance-identifier $instance_rds --region $region --profile $profile
-    echo "Instância $instance_rds startada com sucesso!"
-}
-
-stop_rds() {
-    aws rds start-db-instance --db-instance-identifier $instance_rds --region $region --profile $profile
-    echo "Instância $instance_rds finalizada com sucesso!"
-}
+if [ $current_hour -ge 18 ] || [ $current_hour -lt 6 ]; then
+    stop_ec2
+else
+    echo "Ainda não é hora de parar a instância."
+fi
